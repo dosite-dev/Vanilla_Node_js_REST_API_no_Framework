7d0d13d2b54bdfdb6f8c134671880711e13a9d22
dosite-dev <dositecyiza@gmail.com>
2025-05-01 09:34:17 +0200

    simple routing

--- index.js
+++ index.js
@@ -2,9 +2,14 @@ const http = require('http')
 const products = require('./data/product.js')
 const PORT = process.env.PORT || 5000
 const server = http.createServer((req,res) => {
+    if(req.url === '/api/products' && req.method === 'GET'){
    res.writeHead(200,{'Content-Type':'application/json'})
    res.end(JSON.stringify(products))
-
+}
+else{
+    res.writeHead(404,{'Content-Type':'application/json'})
+   res.end(JSON.stringify({message:'Router not found'}))
+}
 })
 
 
